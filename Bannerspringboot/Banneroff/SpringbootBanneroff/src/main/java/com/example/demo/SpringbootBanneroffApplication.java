7e1126d13bbfae753154ac8e9c671ab9f6fa0bf9
Abhinay20003 <18891a05e8.abhinaykumar@gmail.com>
2024-02-27 17:11:53 +0530

    Added files practice & learnt tday

---
+++ Bannerspringboot/Banneroff/SpringbootBanneroff/src/main/java/com/example/demo/SpringbootBanneroffApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBanneroffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBanneroffApplication.class, args);
+		System.out.println("Hello World to Spring-Boot");
+	}
+
+}
